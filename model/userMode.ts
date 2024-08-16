3bbcc9027d0098afe2001c56d1d20054a20822c9
JecintaCodes <onyemaobijecintaugochi@gmail.com>
2024-08-16 15:55:47 -0700

    first

---
+++ model/userMode.ts
@@ -0,0 +1,79 @@
+import { model, Schema, Types, Document } from "mongoose";
+interface iUser {
+  name: string;
+  email: string;
+  role: string;
+  image?: string;
+  imageID?: string;
+  password: string;
+  secretCode: string;
+  description?: string;
+  products: {}[];
+  myStore: {}[];
+  order: {}[];
+  verify: boolean;
+}
+
+interface iUserData extends iUser, Document {}
+
+const userModel = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      require: true,
+    },
+
+    role: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    imageID: {
+      type: String,
+    },
+
+    description: {
+      type: String,
+    },
+
+    secretCode: {
+      type: String,
+      require: true,
+    },
+
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+
+    myStore: [
+      {
+        type: Types.ObjectId,
+        ref: "prods",
+      },
+    ],
+
+    order: [
+      {
+        type: Types.ObjectId,
+        ref: "orders",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default model<iUserData>("users", userModel);
---
+++ router/productRouter.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import {
+  createProduct,
+  readProduct,
+  readOneProduct,
+  purchaseProduct,
+  viewOrders,
+  deleteProduct,
+} from "../controller/productController";
+import multer from "multer";
+const upload = multer().single("image");
+
+const productRouter = Router();
+
+productRouter.route("/:userID/register-products").post(upload, createProduct);
+productRouter.route("/get-all-product").get(readProduct);
+productRouter.route("/:productID/get-one-product").get(readOneProduct);
+productRouter
+  .route("/:userID/:productID/purchase-product")
+  .post(purchaseProduct);
+
+productRouter.route("/:userID/view-orders").get(viewOrders);
+productRouter.route("/:userID/:productID/delete-product").delete(deleteProduct);
+
+export default productRouter;
